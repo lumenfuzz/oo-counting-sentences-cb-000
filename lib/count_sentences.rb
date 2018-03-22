2f87a1e0c12bc3bfe01129a451d4b16510b5761a
Patrick J Louis <lumenfuzz@gmail.com>
2018-03-22 00:03:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_array = self.scan(/[^\.!?]+[\.!?]/)
+    i = 0
+    sentence_array.each do |sentence|
+      if sentence == "." || sentence == "?" || sentence == "!"
+        #this is not a sentence!
+      else
+        i += 1
+      end
+    end
+      return i
   end
 end
